3f58ac99dff8835efa2aac0ff410025d36cc6260
nekiwo <58634064+nekiwo@users.noreply.github.com>
2026-02-07 14:31:37 -0600

    bedroom env

--- common/control/scripts/PlayerControl.cs
+++ common/control/scripts/PlayerControl.cs
@@ -21,6 +21,7 @@ public partial class PlayerControl : CharacterBody3D
 	public override void _Ready()
 	{
 		_sprite = GetNode<AnimatedSprite3D>("AnimatedSprite3D");
+		_sprite.Play("default");
 	}
 
 	public override void _Process(double delta)
@@ -32,66 +33,66 @@ public partial class PlayerControl : CharacterBody3D
 	{
 		
 
-		var direction = Vector3.Zero;
+		// var direction = Vector3.Zero;
 
-		if (!Locked)
-		{
-			if (Input.IsActionPressed("move_right"))
-			{
-				direction.X += 1.0f;
-			}
-			if (Input.IsActionPressed("move_left"))
-			{
-				direction.X -= 1.0f;
-			}
-			if (Input.IsActionPressed("move_down"))
-			{
-				direction.Z += 1.0f;
-			}
-			if (Input.IsActionPressed("move_up"))
-			{
-				direction.Z -= 1.0f;
-			}
-		}
+		// if (!Locked)
+		// {
+		// 	if (Input.IsActionPressed("move_right"))
+		// 	{
+		// 		direction.X += 1.0f;
+		// 	}
+		// 	if (Input.IsActionPressed("move_left"))
+		// 	{
+		// 		direction.X -= 1.0f;
+		// 	}
+		// 	if (Input.IsActionPressed("move_down"))
+		// 	{
+		// 		direction.Z += 1.0f;
+		// 	}
+		// 	if (Input.IsActionPressed("move_up"))
+		// 	{
+		// 		direction.Z -= 1.0f;
+		// 	}
+		// }
 
-		if (direction != Vector3.Zero)
-		{
-			direction = direction.Normalized();
-		}
+		// if (direction != Vector3.Zero)
+		// {
+		// 	direction = direction.Normalized();
+		// }
 
-		_targetVelocity.X = direction.X * Speed;
-		_targetVelocity.Z = direction.Z * Speed;
+		// _targetVelocity.X = direction.X * Speed;
+		// _targetVelocity.Z = direction.Z * Speed;
 
-		bool onFloor = IsOnFloor();
-		if (onFloor)
-		{
-			if (Input.IsActionPressed("jump"))
-			{
-				_targetVelocity.Y = JumpSpeed;
-			}
-		}
-		else
-		{
-			_targetVelocity.Y -= FallAcceleration * (float)delta;
-		}
+		// bool onFloor = IsOnFloor();
+		// if (onFloor)
+		// {
+		// 	if (Input.IsActionPressed("jump"))
+		// 	{
+		// 		_targetVelocity.Y = JumpSpeed;
+		// 	}
+		// }
+		// else
+		// {
+		// 	_targetVelocity.Y -= FallAcceleration * (float)delta;
+		// }
 
-		_targetVelocity = _targetVelocity.Rotated(Vector3.Up, Rotation.Y);
-		Velocity = _targetVelocity;
+		// _targetVelocity = _targetVelocity.Rotated(Vector3.Up, Rotation.Y);
+		// Velocity = _targetVelocity;
 
-		if (Math.Abs(Velocity.X) >= 0.1)
-		{
-			if (Velocity.X >= 0.1)
-			{
-				_sprite.Play("right");
-			} else if (Velocity.X <= -0.1)
-			{
-				_sprite.Play("left");
-			}
-		} else
-		{
-			_sprite.Play("default");
-		}
+		// if (Math.Abs(Velocity.X) >= 0.1)
+		// {
+		// 	if (Velocity.X >= 0.1)
+		// 	{
+		// 		_sprite.Play("right");
+		// 	} else if (Velocity.X <= -0.1)
+		// 	{
+		// 		_sprite.Play("left");
+		// 	}
+		// } else
+		// {
+		// 	_sprite.Play("default");
+		// }
 			
-		MoveAndSlide();
+		// MoveAndSlide();
 	}
 }
